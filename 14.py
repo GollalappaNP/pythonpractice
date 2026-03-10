7c78579254c582c77fe963821126e0dda63bc6de
GollalappaNP <npgollalappa18@gmail.com>
2026-03-10 22:03:17 +0530

    four

--- 14.py
+++ 14.py
@@ -13,3 +13,16 @@ x = float(input("Enter a number: "))
 if x%2 == 0:
     print("Even")
 else:    print("Odd")
+
+
+#check if a number is a multiple of 5
+x = float(input("Enter a number: "))
+if x%5 == 0:
+    print("its a multiple of 5")
+else:    print("its not a multiple of 5")
+
+
+#check if a number is divisible by both 3 and 5
+x = float(input("Enter a number: "))
+if x%3 == 0 and x%5 == 0:
+    print("its divisible by both 3 and 5")      
